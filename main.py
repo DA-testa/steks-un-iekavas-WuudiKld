2915e68f565479a5bd8c70a406a3af180013d33b
221RDB163 <86880842+WuudiKld@users.noreply.github.com>
2023-02-19 19:31:59 +0200

    result

--- main.py
+++ main.py
@@ -12,20 +12,27 @@ def are_matching(left, right):
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
+
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next, i + 1))
 
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
-
+            if(not opening_brackets_stack or not are_matching(opening_brackets_stack[-1:][0].char, next) ):
+                print(i + 1)
+                return False
+            
+            opening_brackets_stack.pop()
+    
+    if(opening_brackets_stack):
+        print(i+1)
+    
+    return not opening_brackets_stack
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-
+    if(mismatch):
+        print("Success")
 
 if __name__ == "__main__":
     main()
